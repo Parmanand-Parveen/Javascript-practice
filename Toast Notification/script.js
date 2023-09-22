57e519121fe8cf147c8d1c8a109306921105bb99
Parmanand-Parveen <parmanandparveen@gmail.com>
2023-09-22 19:22:26 +0530

    Completed✅

---
+++ Toast Notification/script.js
@@ -0,0 +1,36 @@
+let success = document.querySelector(".success")
+let invalid = document.querySelector(".invalid")
+let error = document.querySelector(".error")
+let toastbox = document.querySelector(".toastbox")
+
+success.addEventListener("click",()=>{
+   let toast = document.createElement("div")
+   toast.innerHTML = `<i class="fa-solid fa-circle-check"></i> Successfully submited`
+  toast.classList.add("toast")
+  toastbox.appendChild(toast)
+  setTimeout(()=>{
+    toast.remove()
+  },5000)
+})
+
+
+invalid.addEventListener("click",()=>{
+    let toast = document.createElement("div")
+    toast.innerHTML = `<i class="fa-solid fa-circle-exclamation"></i> Data is not correct.Check again!`
+   toast.classList.add("toast2","toast")
+   toastbox.appendChild(toast)
+   setTimeout(()=>{
+     toast.remove()
+   },5000)
+ })
+
+ 
+error.addEventListener("click",()=>{
+    let toast = document.createElement("div")
+    toast.innerHTML = `<i class="fa-solid fa-circle-xmark"></i> Error found.`
+   toast.classList.add("toast3","toast")
+   toastbox.appendChild(toast)
+   setTimeout(()=>{
+     toast.remove()
+   },5000)
+ })
