491faa34bb00dc15e08c71c53ede30e9269aecf4
Parmanand-Parveen <parmanandparveen@gmail.com>
2023-10-02 15:41:25 +0530

    Html ✔️

---
+++ Crypto website/script.js
@@ -0,0 +1,8 @@
+let bitcoin = document.querySelector(".bitcoin")
+let ethereum = document.querySelector(".ethereum")
+let dogecoin = document.querySelector(".dogecoin")
+
+async function getData(){
+    let data = await fetch(``)
+
+}
