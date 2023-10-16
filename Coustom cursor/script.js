342dd936cb469debaf6c64cbd3e7324ced69e758
Parmanand-Parveen <parmanandparveen@gmail.com>
2023-10-16 21:12:48 +0530

    Completed✅

--- Coustom cursor/script.js
+++ Coustom cursor/script.js
@@ -1,4 +1,15 @@
+let main = document.querySelector('#main')
+let cursor = document.querySelector(".cursor")
 
+
+main.addEventListener("mousemove",mouse)
+
+
+function mouse(dets){
+    cursor.style.left= dets.x+"px";
+    cursor.style.top= dets.y+"px";
+   
+}
 
 
 
